3c4b4db2ea07d28b5fe6d99efb6e3358d439e525
mahmoudmagdyhassan <107728931+mahmoudmagdyhassan@users.noreply.github.com>
2023-05-13 21:47:45 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,9 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
-# Add a title and intro text
-st.title('Earthquake Data Explorer')
-st.text('This is a web app to allow exploration of Earthquake Data')
+df = pd.DataFrame(
+   np.random.randn(50, 20),
+   columns=('col %d' % i for i in range(20)))
 
-# Create file uploader object
-upload_file = st.file_uploader('Upload a file containing earthquake data')
-
-# Check to see if a file has been uploaded
-if upload_file is not None:
-    # If it has then do the following:
-
-    # Read the file to a dataframe using pandas
-    df = pd.read_csv(upload_file)
-
-    # Create a section for the dataframe statistics
-    st.header('Statistics of Dataframe')
-    st.write(df.describe())
-
-    # Create a section for the dataframe header
-    st.header('Header of Dataframe')
-    st.write(df.head())
-
-    # Create a section for matplotlib figure
-    st.header('Plot of Data')
-    
-    ax.scatter(x=df['Depth'], y=df['Magnitude'])
-    ax.set_xlabel('Depth')
-    ax.set_ylabel('Magnitude')
-    
-    st.pyplot(fig)
+st.dataframe(df)  # Same as st.write(df)
