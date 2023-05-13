8b3db3a13a63ed6a0fca32725524f53411481f9f
mahmoudmagdyhassan <107728931+mahmoudmagdyhassan@users.noreply.github.com>
2023-05-14 01:34:56 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1B-uNksIPuq_evlEgVNOneGD4LLQPTIi-
 """
 
-pip install streamlit
 
 import streamlit as st
 import pandas as pd
