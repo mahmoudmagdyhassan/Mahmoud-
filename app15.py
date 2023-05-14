07019c62ebae0c9d83e9c76607c326241a2759f3
mahmoudmagdyhassan <107728931+mahmoudmagdyhassan@users.noreply.github.com>
2023-05-14 03:37:56 +0300

    Update app15.py

--- app15.py
+++ app15.py
@@ -1,7 +1,17 @@
-import streamlit as st
+import numpy as np
+from sklearn import linear_model
+X = np.random.randn(10, 5)
+y = np.random.randn(10)
+X
+y
 
-st.metric(label="Gas price", value=4, delta=-0.5,
-    delta_color="inverse")
+#clf = linear_model.SGDRegressor( penalty = 'l2' , max_iter=1000, tol=1e-3 , loss = 'huber')
+clf = linear_model.SGDRegressor( penalty = 'l2' , max_iter=1000, tol=1e-3 , loss = 'squared_loss')
+#clf = linear_model.SGDRegressor( penalty = 'l1' , max_iter=1000, tol=1e-3 , loss = 'huber')
+#clf = linear_model.SGDRegressor( penalty = 'l1' , max_iter=1000, tol=1e-3 , loss = 'squared_loss')
 
-st.metric(label="Active developers", value=123, delta=123,
-    delta_color="off")
+clf.fit(X, y)
+clf.score(X,y)
+
+z =  np.random.randn(5)
+print('Predict for ',z , ' is ' , clf.predict(z.reshape(1,-1)))
