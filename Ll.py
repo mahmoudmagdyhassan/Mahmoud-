8446cab976a9481d71eeefbee677556be8e47448
mahmoudmagdyhassan <107728931+mahmoudmagdyhassan@users.noreply.github.com>
2023-05-14 17:07:46 +0300

    Update Ll.py

--- Ll.py
+++ Ll.py
@@ -17,5 +17,6 @@ st.slider('Pick a number', 0,50)
 import matplotlib.pyplot as plt
 import numpy as np
 rand=np.random.normal(1, 2, size=20)
-fig, ax = plt.subplots()ax.hist(rand, bins=15)
+fig, ax = plt.subplots()
+ax.hist(rand, bins=15)
 st.pyplot(fig)
