6e29e77653da54dfe56b52a09050d6ca7054e93e
mahmoudmagdyhassan <107728931+mahmoudmagdyhassan@users.noreply.github.com>
2023-05-14 03:41:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
-import streamlit as st
 import pandas as pd
-import numpy as np
 
-df = pd.DataFrame(
-   np.random.randn(50, 20),
-   columns=('col %d' % i for i in range(20)))
+import streamlit as st
+import streamlit.components.v1 as components
 
-st.dataframe(df)  # Same as st.write(df)
+_selectable_data_table = components.declare_component(
+    "selectable_data_table", url="http://localhost:3001",
+)
+
+
+def selectable_data_table(data, key=None):
+    return _selectable_data_table(data=data, default=[], key=key)
+
+
+raw_data = {
+    "First Name": ["Jason", "Molly", "Tina", "Jake", "Amy"],
+    "Last Name": ["Miller", "Jacobson", "Ali", "Milner", "Smith"],
+    "Age": [42, 52, 36, 24, 73],
+}
+df = pd.DataFrame(raw_data, columns=["First Name", "Last Name", "Age"])
+
+rows = selectable_data_table(df)
+if rows:
+    st.write("You have selected", rows)
