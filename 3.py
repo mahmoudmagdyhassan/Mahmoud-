ccd28eee2ff2c669dc739396ea19ceb19792ed45
mahmoudmagdyhassan <107728931+mahmoudmagdyhassan@users.noreply.github.com>
2023-05-14 03:05:22 +0300

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-import seaborn as sns
+
 
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.preprocessing import StandardScaler, LabelEncoder
