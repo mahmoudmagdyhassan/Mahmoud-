7efd6dbcbbbe39156d73d64bf66cc7c4ed76c34f
mahmoudmagdyhassan <107728931+mahmoudmagdyhassan@users.noreply.github.com>
2023-05-18 04:01:03 +0300

    Update and rename New.py to New.py

--- New.py
+++ New.py
@@ -1,14 +1,15 @@
-
 import streamlit as st
 
-with st.form("my_form"):
-   st.write("Inside the form")
-   slider_val = st.slider("Form slider")
-   checkbox_val = st.checkbox("Form checkbox")
+import pandas as pd
 
-   # Every form must have a submit button.
-   submitted = st.form_submit_button("Submit")
-   if submitted:
-       st.write("slider", slider_val, "checkbox", checkbox_val)
+dataframe = pd.DataFrame({
 
-st.write("Outside the form")
+    'first column': [1, 2, 3, 4],
+
+    'second column': [10, 20, 30, 40],
+
+})
+
+st.experimental_show(dataframe)
+
+
