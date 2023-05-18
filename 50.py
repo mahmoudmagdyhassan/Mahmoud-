da02ae07d897948e0534949cec881d101a65a3ea
mahmoudmagdyhassan <107728931+mahmoudmagdyhassan@users.noreply.github.com>
2023-05-18 05:08:49 +0300

    Update 50.py

--- 50.py
+++ 50.py
@@ -1,5 +1,21 @@
 import streamlit as st
 import pandas as pd
 data =st.file_uploader("Upload a CSV")
-data.head()
-data.info()
+from sklearn.linear_model import LinearRegression
+
+def train_model(data):
+
+	
+
+	X = data.iloc[0,:1]
+
+	y = data[0,1:]
+
+	
+
+	model = LinearRegression()
+
+	model.fit(X, y)
+
+	return model
+
