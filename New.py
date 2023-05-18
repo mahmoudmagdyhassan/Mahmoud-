144c29391a677530978322302d092b73ea6d9cb2
mahmoudmagdyhassan <107728931+mahmoudmagdyhassan@users.noreply.github.com>
2023-05-18 03:30:57 +0300

    Create New.py

---
+++ New.py
@@ -0,0 +1,9 @@
+@st.cache_data
+def show_data():
+    st.header("Data analysis")
+    data = api.get(...)
+    st.success("Fetched data from API!")
+    st.write("Here is a plot of the data:")
+    st.line_chart(data)
+    st.write("And here is the raw data:")
+    st.dataframe(data)
